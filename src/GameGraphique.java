f8fb42fb643f73e99b8da0122574cfea357c0fb6
arnaudden <arnaud.denizet@gmail.com>
2016-03-08 18:27:06 +0100

    Apparition des pigeons

--- src/GameGraphique.java
+++ src/GameGraphique.java
@@ -2,12 +2,18 @@ import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.FlowLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,9 +28,7 @@ public class GameGraphique implements MouseListener {
 
 	private JFrame gameWindow;
 	
-	private JPanel gamePanel;
-	
-	private Canvas gameCanvas;
+	private DisplayPigeon gamePanel;
 	
 	private JPanel menuPanel;
 	
@@ -36,21 +40,21 @@ public class GameGraphique implements MouseListener {
 	
 	private Font fontPrincipal;
 	
-	private ArrayList<Pigeon> listPigeon;
+	
 	
 	public GameGraphique()
 	{
+		
+
 		fontPrincipal = new Font("Arial", java.awt.Font.PLAIN, 16);
 		
 		gameWindow = new JFrame("Pigeon Game");
 		gameWindow.setSize(1000,1000);
 		
 		// Gestion du GamePanel et GameCanvas
-		gamePanel = new JPanel(new BorderLayout());
+		gamePanel = new DisplayPigeon();
 		gamePanel.setSize(1000,800);
-		gameCanvas = new Canvas();
-		gameCanvas.addMouseListener(this);
-		gamePanel.add(gameCanvas);
+		gamePanel.addMouseListener(this);
 		
 		// gestion du MenuPanel
 		
@@ -74,6 +78,7 @@ public class GameGraphique implements MouseListener {
 		
 		gameWindow.add(menuPanel, BorderLayout.NORTH);
 		gameWindow.add(gamePanel);
+		gameWindow.setResizable(false);
 		gameWindow.setVisible(true);
 		
 	}
@@ -87,7 +92,7 @@ public class GameGraphique implements MouseListener {
 			addPigeonOnScreen();
 		}
 		
-		if(e.getSource() == gameCanvas)
+		if(e.getSource() == gamePanel)
 		{
 			addFoodOnScreen();
 		}
@@ -121,10 +126,17 @@ public class GameGraphique implements MouseListener {
 	public void addPigeonOnScreen()
 	{
 		System.out.println("Clique sur add Pigeon");
-		int x = 10 + (int)(Math.random() * ((100 - 10) + 1));
-		int y = 10 + (int)(Math.random() * ((100 - 10) + 1));
 		
-		Pigeon pigeon = new Pigeon( namePigeonTF.getText(), new Vector2D( x, y), new Vector2D( 0, 0));
+		Random randomGen = new Random();
+		int x = 10 + randomGen.nextInt(800);
+		int y = 10 + randomGen.nextInt(600);
+		
+		System.out.println("test 1" + x  + " "+ y);
+		
+		Pigeon pigeon = new Pigeon(namePigeonTF.getText(), new Vector2D(x, y));
+		
+		gamePanel.addPigeon(pigeon);
+		
 	}
 	
 	public void addFoodOnScreen()
@@ -132,4 +144,6 @@ public class GameGraphique implements MouseListener {
 	{
 		System.out.println("Clique n'importe où");
 	}
+	
+	
 }
